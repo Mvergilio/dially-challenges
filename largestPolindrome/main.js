b9794e9b087678adfee953bb9422aaf4093da1ab
Mvergilio <mvergiliodesouza@gmail.com>
2021-12-31 09:42:31 -0300

    palindrome challenge is finished

--- largestPolindrome/main.js
+++ largestPolindrome/main.js
@@ -1,25 +1,31 @@
 function largestPalindromeProduct(n) {
-    const NINE = "9"
+    const NINE = "9";
     let num1 = "",
         num2 = "";
     let polindrome = 0;
     let reversedPolindrome = null;
+    let palindromeArray = []
     for (let i = 0; i < n; i++) {
-
         num1 += NINE;
     }
-    num2 = Number(num1)
+    num2 = Number(num1);
     num1 = num2;
-    console.log(num1, num2)
-    while (polindrome != reversedPolindrome) {
-        polindrome = num1 * num2;
-        reversedPolindrome = polindrome.toString().split("").reverse().join('');
-        reversedPolindrome = Number(reversedPolindrome);
-        num1--;
-        console.log(num1)
+
+    for (num1; num1 > 0; num1--) {
+        let i = num2;
+        for (i; i > 0; i--) {
+            polindrome = num1 * i;
+            reversedPolindrome = polindrome.toString().split("").reverse().join("");
+            reversedPolindrome = Number(reversedPolindrome);
+            if (polindrome == reversedPolindrome) {
+                palindromeArray.push(polindrome);
+            }
+
+        }
+
 
     }
-    console.log(polindrome);
+    polindrome = Math.max(...palindromeArray)
     return polindrome;
 }
 
