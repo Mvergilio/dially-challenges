88ae8592ca7b31f8299b2172a06d4b900fec9723
Mvergilio <mvergiliodesouza@gmail.com>
2022-03-02 09:44:05 -0300

    problem solved

--- primorialOfNumber/index.js
+++ primorialOfNumber/index.js
@@ -2,26 +2,25 @@ function numPrimorial(n) {
     let primoArr = [];
     let primoArrLength = primoArr.length;
     let primeNumber = 2;
-    let check = 0;
+    let count = 0;
     while (primoArrLength < n) {
-        for (let i = 0; i < 100; i++) {
-            let holder = primeNumber % i;
-            if (primeNumber % primeNumber == 0 && primeNumber / 1 == primeNumber && holder > 0) {
-                check++;
+        let check = primeNumber;
+        while (check > 0) {
+            if (primeNumber % check == 0) {
+                count++
+                if (count > 2) {
+                    break;
+                }
             }
+            check--;
         }
-
-        if (check > 2) {
-            break;
-
-        } else if (check <= 2 && !primoArr.includes(primeNumber)) {
-            primoArr.push(primeNumber)
-        }
-        check = 0;
+        if (count <= 2) primoArr.push(primeNumber)
+        count = 0;
         primeNumber++;
         primoArrLength = primoArr.length;
     }
-    console.log(primoArr)
+    let primorialProd = primoArr.reduce((first, next) => first * next);
+    return primorialProd;
 }
-numPrimorial(5);
-// module.exports = numPrimorial;
+numPrimorial(100);
+module.exports = numPrimorial;
