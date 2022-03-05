dafd4b6ab14b0e23a241b34c9b0a4ffcaffa2e7e
Mvergilio <mvergiliodesouza@gmail.com>
2022-03-05 10:43:01 -0300

    found the factor of two number so far

--- smallestMultiple/main.js
+++ smallestMultiple/main.js
@@ -1,47 +1,42 @@
-function smallestMult(n) {
-    let holder = n,
-        arrayOfNumbers = [];
-    while (holder > 0) {
-        arrayOfNumbers.unshift(holder);
-        holder--;
-    }
-    callBackByDivision(arrayOfNumbers);
-}
+// function smallestMult(n) {
+//     let nArr = [];
+//     let i = 1;
+//     while (i <= n) {
+//         nArr.push(i);
+//         i++;
 
-function callBackByDivision(arr) {
-    let product = 1;
-    let result = true;
-    let arrResult;
-    let arrProduct = []
+//     }
+
+// }
+
+function findingFactorOfTwoNum(n1, n2) {
+    let factorArrN1 = [];
+    let factorArrN2 = [];
+    // edge case
+    if (n1 == 1 || n2 == 1) return 1;
+
+    let i = 1;
+
 
     do {
-        arrResult = arr.some((elem) => {
-            return elem % 2 === 0 ? true : false;
-        });
-        if (arrResult) {
-            product = 2;
+        if (n1 % i == 0) {
+            factorArrN1.push(i);
         }
-        if (product % 2 == 0 && product !== 2) {
-            product++;
-        } else if (!arrResult) {
-            arrProduct.push(product);
-            arr = arr.map((element) => {
-                if (element % product == 0) {
-                    return element / product;
-                } else {
-                    return element;
-                }
-            });
-            product++
-        }
-        result = arr.every((elem) => {
-            return elem === 1;
-        });
-        if (product > arr.length) {
-            product = 1;
-        }
+        i++
+    } while (i <= n1);
 
-        console.log(arrProduct, product)
-    } while (result == false);
+
+    i = 1;
+    do {
+
+        if (n2 % i == 0) {
+            factorArrN2.push(i);
+            console.log(i)
+        }
+        i++
+    } while (i <= n2);
+    console.log(factorArrN1, factorArrN2)
 }
-smallestMult(20);
+
+findingFactorOfTwoNum(10, 5)
+// smallestMult(20);
---
+++ smallestMultiple/test.js
@@ -0,0 +1,6 @@
+const assert = require("chai").assert;
+const N_ARR = require('./main');
+
+describe("nArr testing", () => {
+    let nArr = N_ARR;
+})
